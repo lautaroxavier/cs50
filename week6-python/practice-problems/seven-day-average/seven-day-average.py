9f654730bc6d09d6a0fc13e938b0fb33db8a7ea6
Lautaro Rodriguez Xavier <lrodriguezxavier@yahoo.com>
2023-08-25 11:24:54 -0300

    seven-day-average

--- week6-python/practice-problems/seven-day-average/seven-day-average.py
+++ week6-python/practice-problems/seven-day-average/seven-day-average.py
@@ -34,12 +34,40 @@ def main():
 
 # TODO: Create a dictionary to store 14 most recent days of new cases by state
 def calculate(reader):
-    ...
-
+    new_cases = {}
+    prev_cases = {}
+    for reg in reader:
+        state = reg["state"]
+        cases = int(reg["cases"])
+        if state not in new_cases:
+            new_cases[state] = [cases]
+            prev_cases[state] = cases
+        else:
+            new_cases[state] += [cases - prev_cases[state]]
+            prev_cases[state] = cases
+        if len(new_cases[state]) > 14:
+            new_cases[state] = new_cases[state][1:]
+    return new_cases
 
 # TODO: Calculate and print out seven day average for given state
 def comparative_averages(new_cases, states):
-    ...
+
+    for state in states:
+        cur_week_avg = 0
+        last_week_avg = 0
+        daily_cases = new_cases[state]
+        for cases in daily_cases[7:]:
+            cur_week_avg += cases
+        for cases in daily_cases[:7]:
+            last_week_avg += cases
+        cur_week_avg /= 7
+        last_week_avg /= 7
+        dif = cur_week_avg - last_week_avg
+        try:
+            dif /= last_week_avg
+        except ZeroDivisionError:
+            print("Error: division por cero.")
+        print(f"{state} had a 7-day average of {round(cur_week_avg)} cases and a change of {round(dif * 100)}% compared to the last week.")
 
 
 main()
